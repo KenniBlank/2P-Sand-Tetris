d06d006b1de47769599a721c1d3daf968041e09d
KenniBlank <keniblank@gmail.com>
2026-01-25 20:23:41 +0545

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 2P-Sand-Tetris
 Final Project of Computer Programming CT101
+
+
+## Helpful Links
+
+- Why Virtual Resolution is set to what it is set at: https://codecreateplay.com/screen-resolutions-for-game-development/
---
+++ src/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TARGET_FPS 60
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+// TODO: Change to setting.conf file to allow for change depending on display or just set it based on best for current display
+#define VIRTUAL_WIDTH 960
+#define VIRTUAL_HEIGHT 540
+
+#define DEBUG 1
+#endif
